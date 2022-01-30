18a7fc7d248b39f8802fa74d54fdc33ac9e2db3f
Fernando Falcone <fernandofalcone_02@hotmail.com>
2022-01-30 12:50:47 -0300

    Adding header, first components and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UserListComponent } from './pages/users/user-list/user-list.component';
+import { UserFormComponent } from './pages/users/user-form/user-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserListComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
